2ecc7b620faf6c098390e840fa4e3d8fdcea7ef8
Francesca Giordano <francesca020406@hotmail.com>
2017-11-10 23:58:57 -0500

    2nd maps commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+// import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
+  ],
+  providers: [],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
 export class AppComponent {
   title = 'app';
