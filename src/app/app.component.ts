e70149e05672118dae53bb09a2bd78e6f97d3fa4
Francesca Giordano <francesca020406@hotmail.com>
2017-11-11 02:56:03 -0500

    need to fix error with API KEY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,12 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-// import { AppComponent } from './app.component';
-
-import { AgmCoreModule } from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_KEY'
-    })
-  ],
-  providers: [],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBIDNH7AFE4x0-MfJk91-Xu-AMX4a8G1aU'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
